0a5edbb1b4383ddd11864a603ecfa853f7c24822
Daniel Zufrí Quesada <danielzufriquesada@MacBook-Pro-de-Daniel.local>
2024-10-06 17:30:42 +0200

    Represent only best time

--- components/chart.jsx
+++ components/chart.jsx
@@ -11,13 +11,22 @@ export const ChartTest = () => {
 
   // Recalcula las series cada vez que cambie 'times'
   useEffect(() => {
-    const newSeries = users.map(u => ({
-      name: u.label,
-      data: times.filter(t => t.user === u.label).map(tu => tu.time)
-    })).filter(e => e.data.length !== 0);
-
+    const newSeries = users.map(u => {
+      // Filtra los tiempos de cada usuario
+      const userTimes = times.filter(t => t.user === u.label).map(tu => tu.time);
+      
+      // Obtiene el menor tiempo, o undefined si no hay tiempos
+      const minTime = userTimes.length > 0 ? Math.min(...userTimes) : undefined;
+  
+      return {
+        name: u.label,
+        data: minTime !== undefined ? [minTime] : [] // Devuelve un array con el menor tiempo o vacío
+      };
+    }).filter(e => e.data.length !== 0); // Filtra los usuarios sin tiempos
+  
     setSeries(newSeries);
   }, [times]); // El efecto se ejecutará cada vez que cambie 'times'
+  
 
   return (
     <ApexChart
@@ -54,7 +63,7 @@ export const ChartTest = () => {
           curve: 'smooth'
         },
         xaxis: {
-          categories: times.map(t => t.date instanceof Date ? t.date.toLocaleDateString() : t.date?.toDate().toLocaleDateString())
+          categories: [... new Set(times.map(t => t.date instanceof Date ? t.date.toLocaleDateString() : t.date?.toDate().toLocaleDateString()))]
         },
         yaxis: {
           min: 0,
