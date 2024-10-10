e76fdf201eeb443dd8a692a471154065f6aba3b8
Daniel Zufrí Quesada <danielzufriquesada@MacBook-Pro-de-Daniel.local>
2024-10-10 21:36:06 +0200

    Fix Discord link

--- config/site.ts
+++ config/site.ts
@@ -63,7 +63,7 @@ export const siteConfig = {
     github: "https://github.com/nextui-org/nextui",
     twitter: "https://twitter.com/getnextui",
     docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
+    discord: "https://discord.gg/8ADRKxKW",
     sponsor: "https://patreon.com/jrgarciadev",
   },
 };
